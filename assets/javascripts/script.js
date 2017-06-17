0578abcafc13fb433e27ae70782bc6d83067064c
Jawad Shuja <jawad.shuja@synnapps.com>
2017-06-17 07:04:24 +0500

    Add fallback logic

--- assets/javascripts/script.js
+++ assets/javascripts/script.js
@@ -6,27 +6,39 @@ $(document).ready(function() {
   var current_date = new Date().toISOString().slice(0,10);
   $('input[type="date"]').val(current_date);
 
+  if (typeof(Storage) == "undefined") {
+    $('#descriptions-wrap').hide();
+  }
+
   $('#reset-btn').click(function() {
-    localStorage.clear();
+    if (typeof(Storage) !== "undefined") {
+      localStorage.clear();
+    } else {
+      $('.dynamic-list').remove();
+    }
     $('#descriptions-wrap').html('No events found');
   });
 
   $('input[type="date"]').change(function(){
     var date = new Date(this.value);
 
-    if (localStorage.descriptions) {
-      var data = JSON.parse(localStorage['descriptions']);
-      descriptions = data[date.toString()];
+    if (typeof(Storage) !== "undefined") {
+      if (localStorage.descriptions) {
+        var data = JSON.parse(localStorage['descriptions']);
+        descriptions = data[date.toString()];
 
-      if (typeof(descriptions) !== "undefined") {
-        $('#descriptions-wrap').empty();
-        $.each(descriptions, function(index, value) {
-          $('#descriptions-wrap').append(render_event(value));
-        });
-      } else {
-        $('#descriptions-wrap').html('No events found');
+        if (typeof(descriptions) !== "undefined") {
+          $('#descriptions-wrap').empty();
+          $.each(descriptions, function(index, value) {
+            $('#descriptions-wrap').append(render_event(value));
+          });
+        } else {
+          $('#descriptions-wrap').html('No events found');
+        }
       }
-
+    } else {
+      $('.dynamic-list').hide();
+      $('.dynamic-list#list-' + date.toISOString().slice(0,10)).show();
     }
   });
 
@@ -36,7 +48,6 @@ $(document).ready(function() {
     var date = new Date($('input[type="date"]').val());
 
     if (typeof(Storage) !== "undefined") {
-
       if (localStorage.descriptions) {
         var data = JSON.parse(localStorage['descriptions']);
       } else {
@@ -54,6 +65,15 @@ $(document).ready(function() {
       $('#descriptions-wrap').append(render_event(description));
       $('#event-description').val('');
       localStorage['descriptions'] = JSON.stringify(data);
+    } else { // If Local Storage not supported
+      $('.dynamic-list').hide();
+      var selector = '#list-' + date.toISOString().slice(0,10);
+      if ($(selector + ' > li').length > 0) {
+        $(selector).show()
+      } else {
+        $('#descriptions-wrap').after("<ul class='dynamic-list' id='list-" + date.toISOString().slice(0,10) + "'></ul>");
+      }
+      $(selector).append(render_event(description));
     }
 
     return false;
